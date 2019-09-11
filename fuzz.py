6b0807e720e2817883191c548764aef0f1c0e1de
Tony De La Nuez <tony.delanuez@gmail.com>
2019-09-11 06:35:37 -0700

    Adding command list for fuzzer

--- fuzz.py
+++ fuzz.py
@@ -10,22 +10,28 @@ target_host = str(sys.argv[1])
 target_ip = int(sys.argv[2])
 
 buffer_strings = ["A" * 100]
-addition = 100
+addition = 200
+
+# List of commands to fuzz
+command_list = ["USER ", "PASS ", "STATS "]
 
 # Create fuzzing strings in increments of 100
 while len(buffer_strings) <= 50:
     buffer_strings.append("A" * addition)
     addition += 100
 
-# Create connection to service
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((target_host, target_ip))
-
-# Try to crash the program with different length streams
-for buffer in buffer_strings:
-    # Fill this in with potentially vulnerable command, remove newline if needed
-    attack_string = "USER: " + buffer + "\n"
-    print("Sending buffer of length: %s" % len(buffer))
-    s.send(attack_string)
-    response = s.recv(1024)
-    print("Response: %s" % response)
+for command in command_list:
+    # Try to crash the program with different length streams
+    for buffer in buffer_strings:
+        # Create connection to service
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target_host, target_ip))
+        s.recv(1024)
+        # Fill this in with potentially vulnerable command
+        # remove newline if needed
+        attack_string = command + buffer + "\n"
+        print("Sending buffer of length: %s" % len(buffer))
+        s.send(attack_string)
+        response = s.recv(1024)
+        print("Response: %s" % response)
+        s.close()
